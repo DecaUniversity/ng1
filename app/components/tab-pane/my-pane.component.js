f533931d6484983eb8c125c97f702305baec201a
DecaHub <dan@decahub.com>
2017-02-25 20:13:51 -0500

    Complete cross-component communication example for Components

---
+++ app/components/tab-pane/my-pane.component.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const MyPaneController = function () {
+	
+	const ctrl = this;
+	
+	ctrl.$onInit = function () {
+		
+		ctrl.tabsCtrl.addPane(this);
+		console.log(this);
+		
+	}
+	
+};
+
+angular.module("app")
+	.component("myPane", {
+		transclude: true,
+		bindings: {title: "@"},
+		templateUrl: "/components/tab-pane/my-pane.html",
+		controller: MyPaneController,
+		require: {
+			tabsCtrl: "^myTabs"
+		}
+	});
