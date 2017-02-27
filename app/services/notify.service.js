21b75d478e2c9f9ef6e69a49a1932a800c53833d
DecaHub <dan@decahub.com>
2017-02-27 10:17:18 -0500

    Create styles for the Animation pane. Rite theme.

---
+++ app/services/notify.service.js
@@ -0,0 +1,38 @@
+"use strict";
+
+angular.module("app")
+	.controller("ServiceSampleController",
+		["$scope", "notify", function ($scope, notify) {
+		
+			$scope.message = "enter message here";
+			
+			$scope.callNotify = function (msg) {
+				
+				notify(msg);
+				
+			};
+			
+		}]);
+
+angular.module("app")
+	.factory("notify", ["$window", function (win) {
+		
+		let messages = [];
+		const messageLimit = 3;
+		
+		return function (msg) {
+			
+			messages.push(msg);
+			
+			if (messages.length === messageLimit) {
+				
+				win.alert(messages.join("\n"));
+				messages = [];
+				
+			}
+			
+		};
+		
+	}]);
+
+
