6eafcb31b4ad79962572bdef78eb3c57fb77d7f3
DecaHub <dan@decahub.com>
2017-02-28 11:20:06 -0500

    Fixed typo in file name.

---
+++ app/services/convertion.controller.js
@@ -0,0 +1,17 @@
+"use strict";
+
+angular.module("app")
+	.controller("ConversionController",
+		["$scope", "Dec2Bin", function ($scope, converter) {
+		
+			$scope.decimalNumber = "10";
+			
+			$scope.binaryNumber = converter.dec2bin($scope.decimalNumber);
+			
+			$scope.$watch("decimalNumber", function (newValue, oldValue) {
+				
+				$scope.binaryNumber = converter.dec2bin(newValue);
+				
+			});
+			
+		}]);
