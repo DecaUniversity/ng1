f498822ae2a688ac833bfbafaba063d4fe8ef035
DecaHub <dan@decahub.com>
2017-03-02 21:46:07 -0500

    Add note on using provider $sce.DelegateProvider.resourceUrlWhiteList
    Allows to use an URL that points to a resource outside of the origin of the application document.

--- app/scopes/scopes.module.js
+++ app/scopes/scopes.module.js
@@ -3,6 +3,15 @@
 angular.module("scopeExample", [])
 	.config(["$sceDelegateProvider", function ($sceDelegateProvider) {
 		
+		/**
+		 * AngularJS' Strict Contextual Escaping (SCE) mode, enabled by default, blocks
+		 * loading a resource from an insecure URL.
+		 * By default, only URLs that belong to the same origin are trusted. These are
+		 * URLs with the same domain, protocol and port as the application document.
+		 * To use a source from other domains and/or protocols, either adjust the
+		 * whitelist/blacklist or wrap the URL with a call to $sce.trustAsResourceURL
+		 */
+		
 		$sceDelegateProvider.resourceUrlWhitelist([
 			"self",
 			"https://www.youtube.com/embed/**"
