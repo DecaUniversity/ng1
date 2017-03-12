101b7a9609e8bf3c7831924428c3e80e2f8e8711
DecaHub <dan@decahub.com>
2017-03-11 23:41:38 -0400

    Add two directive examples
    Component like directive with isolated scope.
    Decorator directive that uses the link function to manipulate the DOM.

--- app/directives/simple-directive.controller.js
+++ app/directives/simple-directive.controller.js
@@ -8,6 +8,66 @@ angular.module("SimpleDirective")
 			name: "Naomi",
 			address: "1600 Amphitheatre"
 			
+		};
+		
+		$scope.bey = {
+			
+			name: "Queen Bey",
+			address: "Lose Blvd"
+			
+		};
+		
+		$scope.key = {
+			
+			name: "Queen Key",
+			address: "My Blvd"
+			
+		};
+		
+		$scope.mey = {
+			
+			name: "Queen Mey",
+			address: "Breath Blvd"
+			
+		};
+		
+		$scope.format = "hh:mm:ss"
+		
+	}]);
+
+
+angular.module("SimpleDirective")
+	.controller("BeyonceController", ["$scope", function ($scope) {
+		
+		$scope.customer = {
+			
+			name: "Beyoncé Knowles",
+			address: "Destiny's Child Hwy"
+			
+		}
+		
+	}]);
+
+angular.module("SimpleDirective")
+	.controller("KellyController", ["$scope", function ($scope) {
+		
+		$scope.customer = {
+			
+			name: "Kelly Rowland",
+			address: "Destiny's Child Blvd"
+			
+		}
+		
+	}]);
+
+angular.module("SimpleDirective")
+	.controller("MichelleController", ["$scope", function ($scope) {
+		
+		$scope.customer = {
+			
+			name: "Michelle Williams",
+			address: "Destiny's Child Lane"
+			
 		}
 		
 	}]);
